b604da56f3d444d1c3afead1c7cd7e778bc5cbb8
jurciusmantas <jurciusmantas@gmail.com>
2018-12-13 01:28:13 +0200

    QrActivity -> UserInfoActivity

--- VirtualLibrarity/Activities/UserInfoActivity.cs
+++ VirtualLibrarity/Activities/UserInfoActivity.cs
@@ -1,26 +1,35 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
 using Newtonsoft.Json;
 using VirtualLibrarity.Models;
+using ZXing.Mobile;
 
 namespace VirtualLibrarity
 {
     [Activity(Label = "ManualLoginActivity")]
     public class UserInfoActivity : Activity
     {
-        UserToLoginResponse2 user;
-        LinearLayout _container;
+        private UserToLoginResponse2 user;
+        private LinearLayout _container;
+        private RequestSender _RequestSender = new RequestSender();
+        private Button _buttonTake;
+        private Button _buttonReturn;
+        private MobileBarcodeScanner _scanner;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.activity_logged_in);
+            SetContentView(Resource.Layout.activity_user_info);
 
             string userString = Intent.GetStringExtra("user");
             user = JsonConvert.DeserializeObject<UserToLoginResponse2>(userString);
+            MobileBarcodeScanner.Initialize(Application);
+            _scanner = new MobileBarcodeScanner();
 
             TextView NameTV = FindViewById<TextView>(Resource.Id.infoUserNameTV);
             NameTV.Text += user.UserInfo.Firstname;
@@ -29,14 +38,43 @@ namespace VirtualLibrarity
             TextView EmailTV = FindViewById<TextView>(Resource.Id.infoUserEmailTV);
             EmailTV.Text += user.UserInfo.Email;
 
+            _buttonTake = FindViewById<Button>(Resource.Id.btnTake);
+            _buttonReturn = FindViewById<Button>(Resource.Id.btnReturn);
+
+            _buttonTake.Click += async delegate
+                {
+                    _scanner.UseCustomOverlay = false;
+                    _scanner.TopText = "Hold the camera up to the barcode\nAbout 15 centimeters away";
+                    _scanner.BottomText = "Wait for the barcode to automatically scan!";
+                    var result = await _scanner.Scan();
+
+                    HandleScanResultBorrow(result);
+                };
+
+            _buttonReturn.Click += async delegate 
+            {
+                _scanner.UseCustomOverlay = false;
+                _scanner.TopText = "Hold the camera up to the barcode\nAbout 15 centimeters away";
+                _scanner.BottomText = "Wait for the barcode to automatically scan!";
+                var result = await _scanner.Scan();
+                HandleScanResultReturn(result);
+            };
+
             AddBooksToReturnList();
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            if (ZXing.Net.Mobile.Android.PermissionsHandler.NeedsPermissionRequest(this))
+                ZXing.Net.Mobile.Android.PermissionsHandler.RequestPermissionsAsync(this);
         }
 
         private void AddBooksToReturnList()
         {
             _container = FindViewById<LinearLayout>(Resource.Id.container);
 
-            if (user.BorrowedBooks.Count> 0)
+            if (user.BorrowedBooks.Count > 0)
             {
                 foreach (Book bookModel in user.BorrowedBooks)
                 {
@@ -55,6 +93,46 @@ namespace VirtualLibrarity
                     _container.AddView(addView);
                 }
             }
+        }
+
+        void HandleScanResultBorrow(ZXing.Result result)
+        {
+            string msg = "";
+
+            if (result != null && !string.IsNullOrEmpty(result.Text))
+                msg = "Found Barcode: " + result.Text;
+            else
+                msg = "Scanning Canceled!";
+
+            this.RunOnUiThread(() => Toast.MakeText(this, msg, ToastLength.Short).Show());
+
+            _RequestSender.SendBookRequest(result.Text, true);
+            //istrinti is bibliotekos knygu saraso (pagal barcode, kuris yra result.Text)
+            //prideti prie user knygu saraso
+
+
+        }
+
+        void HandleScanResultReturn(ZXing.Result result)
+        {
+            string msg = "";
+
+            if (result != null && !string.IsNullOrEmpty(result.Text))
+                msg = "Found Barcode: " + result.Text;
+            else
+                msg = "Scanning Canceled!";
+
+            this.RunOnUiThread(() => Toast.MakeText(this, msg, ToastLength.Short).Show());
+            _RequestSender.SendBookRequest(result.Text, false);
+            //istrinti is user knygu saraso (pagal barcode, kuris yra result.Text
+            //prideti atga prie bibliotekos knygu saraso
+
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
