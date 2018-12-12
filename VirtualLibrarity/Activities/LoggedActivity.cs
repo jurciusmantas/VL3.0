06859991dbff06d0b584eb6024829d88d3ec0ebc
aurelijacygaite <cygau@LAPTOP-5AFKC39K>
2018-12-12 23:16:33 +0200

    client changes

---
+++ VirtualLibrarity/Activities/LoggedActivity.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+
+namespace VirtualLibrarity.Activities
+{
+    [Activity(Label = "LoggedInActivity")]
+    public class LoggedActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_logged);
+
+            ImageButton logOutButton = FindViewById<ImageButton>(Resource.Id.logOutButton);
+            Button borrowedBooksButton = FindViewById<Button>(Resource.Id.borrowedBooksButton);
+            Button takeOrReturnBookQRButton = FindViewById<Button>(Resource.Id.QRButton);
+            Button availableBooksButton = FindViewById<Button>(Resource.Id.AvailableBooksButton);
+            TextView userNameTV = FindViewById<TextView>(Resource.Id.infoUserNameTV);
+
+
+            User userToLogin;
+            //userToLogin = JsonConvert.DeserializeObject<User>(Intent.GetStringExtra("User"));
+           // userNameTV.Text = userToLogin.FirstName;
+
+            //PRIDERI PRIE userNameTV.Text vartotojo varda
+
+            logOutButton.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            };
+
+            borrowedBooksButton.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(UserInfoActivity));
+               // intent.PutExtra("User", JsonConvert.SerializeObject(userToLogin)); //perkelti user duomenis
+                StartActivity(intent);
+            };
+
+            takeOrReturnBookQRButton.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(QrActivity));
+               // intent.PutExtra("User", JsonConvert.SerializeObject(userToLogin)); //perkelti user duomenis
+                StartActivity(intent);
+            };
+
+            availableBooksButton.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(AvailableBooksActivity));
+               // intent.PutExtra("User", JsonConvert.SerializeObject(userToLogin)); //perkelti user duomenis
+                StartActivity(intent);
+            };
+        }
+
+    }
+}
--- VirtualLibrarity/Activities/LoginProxyActivity.cs
+++ VirtualLibrarity/Activities/LoginProxyActivity.cs
@@ -51,7 +51,7 @@ namespace VirtualLibrarity.Activities
         }
         private void TryUserIfNull()
         {
-            if (user == null)
+            if (user.UserInfo == null)
             {
                 Toast.MakeText(this, "Did not recieve response", ToastLength.Long).Show();
                 GoBack();
@@ -70,7 +70,7 @@ namespace VirtualLibrarity.Activities
             }
             else
             {
-                Intent intent = new Intent(this, typeof(LoggedInActivity));
+                Intent intent = new Intent(this, typeof(UserInfoActivity));
                 string userString = JsonConvert.SerializeObject(user);
                 intent.PutExtra("user", userString);
                 StartActivity(intent);
