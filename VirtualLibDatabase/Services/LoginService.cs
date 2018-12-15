0b46d76772c0952e28f56e0ee846f7f5aa28cd8a
jurciusmantas <jurciusmantas@gmail.com>
2018-12-15 23:01:40 +0200

    register and login

--- VirtualLibDatabase/Services/LoginService.cs
+++ VirtualLibDatabase/Services/LoginService.cs
@@ -1,6 +1,28 @@
-﻿namespace VirtualLibDatabase.Services
+﻿using System.Linq;
+
+namespace VirtualLibDatabase.Services
 {
     public class LoginService
     {
+        public users ManualLogin(string email, string password)
+        {
+            using(var context = new vlEntities())
+            {
+                return context.users
+                    .Where(u => u.Email == email)
+                    .Where(u => u.Password == password)
+                    .FirstOrDefault();
+            }
+        }
+
+        public users FaceRecognitionLogin(int id)
+        {
+            using(var context = new vlEntities())
+            {
+                return context.users
+                    .Where(u => u.Id == id)
+                    .FirstOrDefault();
+            }
+        }
     }
 }
