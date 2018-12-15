8b478c4bf923a113b540ca8df150fd45c24f672d
jurciusmantas <jurciusmantas@gmail.com>
2018-12-15 21:10:40 +0200

    Entity framework init

---
+++ VirtualLibDatabase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new vlEntities())
+            {
+                Console.WriteLine(context.users.Where(u => u.Email == "j")
+                    .FirstOrDefault().Id);
+                Console.ReadKey();
+            }
+        }
+    }
+}
