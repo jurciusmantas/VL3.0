91c49c68dada4efacc1ccfe107b4b357558fc624
jurciusmantas <jurciusmantas@gmail.com>
2018-12-16 19:27:15 +0200

    Forgot interfaces

---
+++ VirtualLibAPI/Services/ILoginService.cs
@@ -0,0 +1,11 @@
+﻿using VirtualLibrarity.EFModel;
+
+namespace VirtualLibAPI.Services
+{
+    public interface ILoginService
+    {
+        users ManualLogin(string email, string password);
+
+        users FaceRecognitionLogin(int id);
+    }
+}
